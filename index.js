cb50ae8dcc8c640cf5f731765b5c48d221a9e1cc
sepuckett86 <sepuckett86@gmail.com>
2019-06-24 16:06:05 -0700

    add function backbones to index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+function map(arr, callback) {
+  // fill in
+}
+
+function filter(arr, callback) {
+  // fill in
+}
+
+function findIndex(arr, callback) {
+  // fill in
+}
+
+function reduce(arr, callback, [initialValue]) {
+  // fill in
+}
+
+function every(arr, callback) {
+  // fill in
+}
+
+function forEach(arr, callback) {
+  // fill in
+}
+
+module.exports = { 
+  map,
+  filter,
+  findIndex,
+  reduce,
+  every,
+  forEach
+}
