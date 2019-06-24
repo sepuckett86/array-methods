1be4871e9e12cc3a975fc2b33e2d56bf40ea77aa
sepuckett86 <sepuckett86@gmail.com>
2019-06-24 16:29:59 -0700

    add another test for map

--- index.test.js
+++ index.test.js
@@ -3,11 +3,18 @@ const { map, filter, findIndex, reduce, every, forEach } = require('./index.js')
 const arr = [1, 2, 3, 4, 5];
 
 describe('index functions', () => {
-  it('can map', () => {
+  it('can map (n + 1)', () => {
     const myCallback = (number) => number + 1;
     const updatedArray = map(arr, myCallback);
     expect(updatedArray).toEqual([
       2, 3, 4, 5, 6
+    ]);
+  });
+  it('can map (n - 1)', () => {
+    const myCallback = (number) => number - 1;
+    const updatedArray = map(arr, myCallback);
+    expect(updatedArray).toEqual([
+      0, 1, 2, 3, 4
     ]);
   });
 });
