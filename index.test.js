0116dd1060fc58dbe3ae3d1c359be58b4842cb70
sepuckett86 <sepuckett86@gmail.com>
2019-06-24 16:33:11 -0700

    remove parentheses in test

--- index.test.js
+++ index.test.js
@@ -4,14 +4,14 @@ const arr = [1, 2, 3, 4, 5];
 
 describe('index functions', () => {
   it('can map (n + 1)', () => {
-    const myCallback = (number) => number + 1;
+    const myCallback = number => number + 1;
     const updatedArray = map(arr, myCallback);
     expect(updatedArray).toEqual([
       2, 3, 4, 5, 6
     ]);
   });
   it('can map (n - 1)', () => {
-    const myCallback = (number) => number - 1;
+    const myCallback = number => number - 1;
     const updatedArray = map(arr, myCallback);
     expect(updatedArray).toEqual([
       0, 1, 2, 3, 4
